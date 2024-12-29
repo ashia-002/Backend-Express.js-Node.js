0e5deab7338e5f30c7a5197fe03bf3a7bce9d962
Ashia Sultana <ashia.sultana.maisha@gmail.com>
2024-12-29 20:46:04 +0600

    Updated server.js file.

--- server/server.js
+++ server/server.js
@@ -1,6 +1,14 @@
 const express = require("express");
 const app = express();
 
+const PORT = 5000
+
 app.get("/", () => {
     res.status(200).send("Welcome");
 });
+
+app.listen(PORT, (req, res) => {
+    console.log('server is running.')
+})
+
+
