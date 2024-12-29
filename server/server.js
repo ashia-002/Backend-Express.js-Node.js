cb36b12bb7bb34348709ce9545b769fc10815a30
Ashia Sultana <ashia.sultana.maisha@gmail.com>
2024-12-29 20:48:58 +0600

    Updated the server.js file.(added get register).

--- server/server.js
+++ server/server.js
@@ -7,6 +7,10 @@ app.get("/", () => {
     res.status(200).send("Welcome");
 });
 
+app.get("/register", () => {
+    res.status(200).send("Do your registraion in this page.");
+});
+
 app.listen(PORT, (req, res) => {
     console.log('server is running.')
 })
