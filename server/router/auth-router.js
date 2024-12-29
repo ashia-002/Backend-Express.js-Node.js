7ea9a2055f27192a4b62efca34b55a1f6021c796
Ashia Sultana <ashia.sultana.maisha@gmail.com>
2024-12-29 21:54:57 +0600

    Added new way to define route.

--- server/router/auth-router.js
+++ server/router/auth-router.js
@@ -3,7 +3,11 @@ const router = express.Router();
 const { home, login } = require("../controllers/auth-controller");
 
 // Define the routes with their respective controllers
-router.get("/", home); // Handles /api/auth
-router.get("/login", login); // Handles /api/auth/login
+// router.get("/", home); // Handles /api/auth
+// router.get("/login", login); // Handles /api/auth/login
+
+router.route("/").get(home); // Handles /api/auth
+router.route("/login").get(login); // Handles /api/auth/login
+
 
 module.exports = router;
